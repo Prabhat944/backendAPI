ecfbb247f5eb31ee8ed822966cfcb0a2188085f7
Prabhat Kumar <prabhatkumar944@gmail.com>
2025-07-02 09:06:19 +0530

    changes in db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017,localhost:27018,localhost:27019/fantsy11?replicaSet=rs0");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB Connected:', process.env.MONGO_URI);
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error.message);
